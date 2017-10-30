be516f583fffe4c6062b1d0fae05c8221a8b94f7
samira <s.soltani336@gmail.com>
2017-10-30 18:30:10 +0800

    Add show method and the corresponding view for the abouts controller

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -1,4 +1,9 @@
 class AboutController < ApplicationController
   def index
+    @myabouttext = 'This comes from the about controller'
+  end
+
+  def show
+    @myaboutshowtext = 'This comes from the about#show method'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -17,7 +17,7 @@ Rails.application.routes.draw do
 
   get 'posts/show'
 
-  get 'about/index'
+  get 'aboutshow' => "about#show"
 
   get "/about" => "about#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
